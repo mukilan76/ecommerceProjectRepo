48e9a6f0d96996bfa115e000855b911a6ce2260f
mukilan76 <mukilanr2001@gmail.com>
2025-05-11 21:16:32 +0530

    a

--- project/src/main/java/com/example/demo/entity/User.java
+++ project/src/main/java/com/example/demo/entity/User.java
@@ -28,7 +28,7 @@ public class User {
 	@Column(nullable = false, unique = true)
 	private String email;
 	@Column(nullable = false)
-	private String password;
+	private String password; 
 	private String name;
 	private String phone;
 	private boolean isAdmin;
